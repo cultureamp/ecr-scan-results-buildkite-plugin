98e783374551da9f430626dc753cbbea1ec1bc8d
James Telfer <792299+jamestelfer@users.noreply.github.com>
2022-03-17 21:33:56 +1100

    Implement plugin functionality in Go
    A proof of concept for implementing a Buildkite plugin in a different
    language. This came out of realising that implementing a report via jq
    wasn't that maintainable, and other options just highlight that Bash
    isn't a good fit as the needs became more complicated.
    
    The idea here is to ship the plugin as a binary via Docker, skipping the
    need to build on the agent.
    
    Don't fail a build when the plugin fails

---
+++ src/runtimeerrors/error_test.go
@@ -0,0 +1,50 @@
+package runtimeerrors_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cultureamp/ecrscanresults/runtimeerrors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIIsFatal(t *testing.T) {
+	cases := []struct {
+		test   string
+		result bool
+		err    error
+	}{
+		{
+			test:   "true when nil",
+			result: true,
+			err:    nil,
+		},
+		{
+			test:   "true on normal error",
+			result: true,
+			err:    assert.AnError,
+		},
+		{
+			test:   "false on nonfatal error",
+			result: false,
+			err:    runtimeerrors.NonFatalError{},
+		},
+		{
+			test:   "false on wrapped nonfatal error",
+			result: false,
+			err:    fmt.Errorf("wrapped %w", runtimeerrors.NonFatal("end of the line", nil)),
+		},
+		{
+			test:   "false on nonfatal error in chain",
+			result: false,
+			err:    fmt.Errorf("wrapped %w", runtimeerrors.NonFatal("wrapped further", assert.AnError)),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.test, func(t *testing.T) {
+			result := runtimeerrors.IsFatal(c.err)
+			assert.Equal(t, c.result, result)
+		})
+	}
+}
