d01fc444ad6224786af95181fa61fcc4e82b1d46
James Telfer <792299+jamestelfer@users.noreply.github.com>
2023-11-22 09:51:30 +1100

    feat: render ignored findings in separate table

--- src/report/annotation_test.go
+++ src/report/annotation_test.go
@@ -111,26 +111,11 @@ func TestReports(t *testing.T) {
 				ImageLabel: "label of image",
 				FindingSummary: finding.Summary{
 					Counts: map[types.FindingSeverity]finding.SeverityCount{
-						"HIGH":              {Included: 1},
-						"AA-BOGUS-SEVERITY": {Included: 0, Ignored: 1},
-						"CRITICAL":          {Included: 1},
+						"HIGH":     {Included: 1},
+						"CRITICAL": {Included: 1, Ignored: 1},
+						"LOW":      {Included: 0, Ignored: 1},
 					},
 					Details: []finding.Detail{
-						{
-							Name:           "CVE-2019-5300",
-							Description:    "Another vulnerability.",
-							Uri:            "http://people.ubuntu.com/~ubuntu-security/cve/CVE-2019-5300",
-							Severity:       "AA-BOGUS-SEVERITY",
-							PackageName:    "5300-package",
-							PackageVersion: "5300-version",
-							CVSS2Score:     "10.0",
-							CVSS2Vector:    "AV:L/AC:L/Au:N/C:P/I:P/A:P",
-							Ignore: &findingconfig.Ignore{
-								ID:     "CVE-2019-5300",
-								Until:  findingconfig.MustParseUntil("2023-12-31"),
-								Reason: "ignored",
-							},
-						},
 						{
 							Name:           "CVE-2019-5188",
 							Description:    "A code execution vulnerability exists in the directory rehashing functionality of E2fsprogs e2fsck 1.45.4. A specially crafted ext4 directory can cause an out-of-bounds write on the stack, resulting in code execution. An attacker can corrupt a partition to trigger this vulnerability.",
@@ -152,7 +137,36 @@ func TestReports(t *testing.T) {
 							CVSS2Vector:    "AV:L/AC:L/Au:N/C:P/I:P/A:P",
 						},
 					},
-					Ignored: []finding.Detail{},
+					Ignored: []finding.Detail{
+						{
+							Name:           "CVE-2023-100",
+							Description:    "A vulnerability present in some software but isn't that bad.",
+							Uri:            "http://people.ubuntu.com/~ubuntu-security/cve/CVE-2023-100",
+							Severity:       "LOW",
+							PackageName:    "100-package",
+							PackageVersion: "100-version",
+							CVSS2Score:     "4.0",
+							CVSS2Vector:    "AV:L/AC:L/Au:N/C:P/I:P/A:P",
+							Ignore: &findingconfig.Ignore{
+								ID: "CVE-2023-100",
+							},
+						},
+						{
+							Name:           "CVE-2019-5300",
+							Description:    "Another vulnerability.",
+							Uri:            "http://people.ubuntu.com/~ubuntu-security/cve/CVE-2019-5300",
+							Severity:       "CRITICAL",
+							PackageName:    "5300-package",
+							PackageVersion: "5300-version",
+							CVSS2Score:     "10.0",
+							CVSS2Vector:    "AV:L/AC:L/Au:N/C:P/I:P/A:P",
+							Ignore: &findingconfig.Ignore{
+								ID:     "CVE-2019-5300",
+								Until:  findingconfig.MustParseUntil("2023-12-31"),
+								Reason: "Ignored to give the base image a chance to be updated",
+							},
+						},
+					},
 				},
 				CriticalSeverityThreshold: 0,
 				HighSeverityThreshold:     0,
